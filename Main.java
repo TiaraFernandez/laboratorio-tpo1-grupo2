b80c2185d35548980984ce9407f9662df5316501
Maia Maripil <maia.maripil@est.fi.uncoma.edu.ar>
2026-09-01 11:32:06 -0300

    Agrego cambios en el main: nuevas tareas callables y mejoras en el codigo

--- Main.java
+++ Main.java
@@ -8,68 +8,93 @@ public class Main {
         List<Indumentaria> prendasList = new ArrayList<>();
 
         EstiloRockero rockera = new EstiloRockero();
-        EstiloPreppy preppy= new EstiloPreppy();
+        EstiloPreppy preppy = new EstiloPreppy();
+        Estilo[] arrayEstilo = { preppy, rockera };
 
-        ExecutorService ejecutor = Executors.newFixedThreadPool(2);
+        ExecutorService ejecutor = Executors.newFixedThreadPool(4);
+        CountDownLatch pestillo = new CountDownLatch(3);
 
         /* Se crean los callables con las tareas a realizar */
-        Callable<String> generarPrendas = () ->{
-            int indumentaria;
+        Callable<List<Indumentaria>> generarPrendasArriba = () -> {
+            List<Indumentaria> parteArribaList = new ArrayList<>();
+            int estilo, prenda;
+            String[] tipoPrenda = { "Remera", "Abrigo", "Musculosa" };
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++) {
+                estilo = random.nextInt(2);
+                prenda = random.nextInt(3);
+
+                parteArribaList.add(new ParteDeArriba(tipoPrenda[prenda], arrayEstilo[estilo]));
+            }
+            pestillo.countDown();
+            return parteArribaList;
+        };
+
+        Callable<List<Indumentaria>> generarPrendasAbajo = () -> {
+            List<Indumentaria> parteAbajoList = new ArrayList<>();
+            int estilo, prenda;
+            String[] tipoPrenda = { "Pantalon", "Short", "Pollera" };
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++) {
+                estilo = random.nextInt(2);
+                prenda = random.nextInt(3);
+
+                parteAbajoList.add(new ParteDeAbajo(tipoPrenda[prenda], arrayEstilo[estilo]));
+
+            }
+            pestillo.countDown();
+            return parteAbajoList;
+        };
+
+        Callable<List<Indumentaria>> generarPrendasVestido = () -> {
+            List<Indumentaria> vestidoList = new ArrayList<>();
             int estilo;
             Random random = new Random();
 
-            for (int i = 0; i < 20; i++) {
-                indumentaria = random.nextInt(3);
+            for (int i = 0; i < 10; i++) {
                 estilo = random.nextInt(2);
 
-                switch (indumentaria) {
-                    case 1:
-                        if (estilo == 0) {
-                            prendasList.add(new ParteDeArriba("Remera", preppy));
-                        }else{
-                            prendasList.add(new ParteDeArriba("Remera", rockera));
-                        }
-                        break;
-                    case 2:
-                        if (estilo == 0) {
-                            prendasList.add(new ParteDeAbajo("Pantalon", preppy));
-                        }else{
-                            prendasList.add(new ParteDeAbajo("Pantalon", rockera));
-                        }
-                        break;
-                    default:
-                        if (estilo == 0) {
-                            prendasList.add(new Vestido(preppy));
-                        }else{
-                            prendasList.add(new Vestido(rockera));
-                        }
-                        break;
-                }
+                vestidoList.add(new Vestido(arrayEstilo[estilo]));
+
             }
-            return "Prendas generadas con exito.";
+            pestillo.countDown();
+            return vestidoList;
         };
 
-        Callable<String> mostrarPrendas = () ->{ /* Imprimo la lista de las prendas */
+        Future<List<Indumentaria>> fArriba = ejecutor.submit(generarPrendasArriba);
+        Future<List<Indumentaria>> fAbajo = ejecutor.submit(generarPrendasAbajo);
+        Future<List<Indumentaria>> fVestidos = ejecutor.submit(generarPrendasVestido);
+
+        Callable<String> mostrarPrendas = () -> { /* Imprimo la lista de las prendas */
+            pestillo.await(); // espera hasta que terminen de armarse las sublistas
+            prendasList.addAll(fAbajo.get());
+            prendasList.addAll(fArriba.get());
+            prendasList.addAll(fVestidos.get());
+
             System.out.println("La lista de prendas de moda generada es: ");
             for (Indumentaria prenda : prendasList) {
-                System.out.println("\t"+prenda.confeccionar());
+                System.out.println("\t" + prenda.confeccionar());
             }
             return "Se mostraron todas las prendas con exito.";
         };
 
         /* Se envian las tareas el executor y recibimos los futures correspondientes */
-        Future<String> futureGenerar = ejecutor.submit(generarPrendas);
+
         Future<String> futureMostrar = ejecutor.submit(mostrarPrendas);
 
         try {
             String resultado;
-            resultado = futureGenerar.get();
-            System.out.println(resultado);
+            // resultado = futureGenerar.get();
+            // System.out.println(resultado);
             resultado = futureMostrar.get();
-            
+            System.out.println(resultado);
+
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             ejecutor.shutdown();
         }
     }
